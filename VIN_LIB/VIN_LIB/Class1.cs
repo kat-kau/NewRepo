4b658cc08e539f08aef1a0b65fa40077f4a18ef3
Ekaterina Spasova <katkau2012@gmail.com>
2020-02-12 17:33:12 +0500

    VIN_LIB

---
+++ VIN_LIB/VIN_LIB/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VIN_LIB
+{
+    public class Class1
+    {
+        bool CheckVIN(string vin)
+        {
+            return true;
+        }
+
+        string GetVINCountry(string vin)
+        {
+            string Country = "";
+            return Country;
+        }
+
+
+        int GetTransportYear(string vin)
+        {
+            int year = 0;
+            return year;
+        }
+    }
+}
