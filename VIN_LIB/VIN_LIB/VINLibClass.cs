551c29852cabe6c2392241e8e1365105d932730e
Ekaterina Spasova <katkau2012@gmail.com>
2020-02-13 17:48:48 +0500

    1

--- VIN_LIB/VIN_LIB/VINLibClass.cs
+++ VIN_LIB/VIN_LIB/VINLibClass.cs
@@ -15,18 +15,18 @@ namespace VIN_LIB
 
             string vinHash = vin.Replace('A','1').Replace('B','2').Replace('C','3').Replace('D', '4').Replace('E', '5').Replace('F', '6').Replace('G', '7').Replace('H', '8').Replace('J', '1').Replace('K', '2').Replace('L', '3').Replace('M', '4').Replace('N', '5').Replace('P', '7').Replace('R', '9').Replace('S', '2').Replace('T', '3').Replace('U', '4').Replace('V', '5').Replace('W', '6').Replace('X', '7').Replace('Y', '8').Replace('Z', '9');
 
-            char[] vinNumChar;
+            // vinHash = string.Join("", vinHash.Select(x => x + " "));
 
-            int[] vinNumInt;
+            int[] position = { };
+            int[] vinHashNum = Array.ConvertAll(vinHash.Split(), int.Parse);
 
-            vinNumChar = vin.ToCharArray();
-
-            for (int i = 0; i < vinNumChar.Length; i++)
+            int nineNumber;
+            for (int i = 0; i <= vinHashNum.Length; i++)
             {
-               
+                nineNumber = vinHashNum[i] 
             }
 
-                if (Regex.IsMatch(vin, pattern))
+            if (Regex.IsMatch(vin, pattern))
             {
                 return true;
             } else return false;
